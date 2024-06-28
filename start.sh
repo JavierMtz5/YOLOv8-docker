9c93522739892d3d8f0caf5f32c967cc1a9def71
JavierMtz5 <jmartinezojeda5upv@gmail.com>
2024-06-28 14:32:37 +0200

    start.sh bash script added

---
+++ start.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Variables
+IMAGE_NAME="ml_app"
+IMAGE_TAG="0.0.3"
+FULL_IMAGE_NAME="${IMAGE_NAME}:${IMAGE_TAG}"
+LOCAL_INFERENCE_RESULTS_DIR="/home/jmartinez/ml_app/results"
+DOCKER_INFERENCE_RESULTS_DIR="/usr/src/ultralytics/runs"
+
+# Stop and remove any previous containers running from the given image
+echo "Stopping and removing any existing containers for the image ${FULL_IMAGE_NAME}..."
+CONTAINER_IDS=$(docker ps -a -q --filter ancestor=${FULL_IMAGE_NAME})
+
+if [ -n "$CONTAINER_IDS" ]; then
+  docker stop $CONTAINER_IDS
+  docker rm $CONTAINER_IDS
+  echo "Stopped and removed containers: $CONTAINER_IDS"
+else
+  echo "No containers found for image ${FULL_IMAGE_NAME}."
+fi
+
+# Remove the previous Docker image if it exists
+echo "Removing the Docker image ${FULL_IMAGE_NAME} if it exists..."
+IMAGE_IDS=$(docker images -q ${FULL_IMAGE_NAME})
+
+if [ -n "$IMAGE_IDS" ]; then
+  docker rmi $IMAGE_IDS
+  echo "Removed image: $IMAGE_IDS"
+else
+  echo "No image found with the name ${FULL_IMAGE_NAME}."
+fi
+
+# Build the docker image from the Dockerfile
+echo "Building the Docker image ${FULL_IMAGE_NAME}..."
+docker build -t ${FULL_IMAGE_NAME} .
+
+# Run the docker image
+echo "Running the Docker image ${FULL_IMAGE_NAME}..."
+docker run -d --ipc=host --gpus all -p 8080:8080 -v $LOCAL_INFERENCE_RESULTS_DIR:$DOCKER_INFERENCE_RESULTS_DIR ${FULL_IMAGE_NAME}
+
+echo "Docker container is up and running."

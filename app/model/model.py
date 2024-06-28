9e30db239c593f3cfba8e9bee2e7fb30f51f3240
JavierMtz5 <jmartinezojeda5upv@gmail.com>
2024-06-29 01:51:38 +0200

    improved metadata returned in inference responses, and added new function for inference on a single image

--- app/model/model.py
+++ app/model/model.py
@@ -1,7 +1,7 @@
 import os
-from typing import Dict, Union, List, Any
-from datetime import datetime
+from typing import Dict, List, Any
 import json
+from PIL import Image
 from pathlib import Path
 
 import ultralytics
@@ -18,38 +18,25 @@ MODEL_PATH = os.path.join(BASE_DIR, 'model_weights', model_weights_filename)
 
 model = ultralytics.YOLO(MODEL_PATH)
 
-def inference(imgs_path: str) -> List[Dict[str, Any]]:
+def inference_on_path(imgs_path: str) -> List[Dict[str, Any]]:
     """Performs inference on the trained model for the YOLOv8 architecture"""
     results: ultralytics.engine.results.Results = model(source=imgs_path, show=False, save=True, conf=0.45)
     json_results = []
     for result in results:
-        json_results.append(json.loads(result.tojson()))
+        result_metadata = {
+            'shape': result.orig_shape,
+            'path': result.path,
+            'detections': json.loads(result.tojson())
+        }
+        json_results.append(result_metadata)
 
     return json_results
 
-# def parse_inference_results(results: ultralytics.engine.results.Results) -> List[Dict[str, Any]]:
-#     """Converts the results obtained from the ultralytics inference"""
-#     results_data = []
-#     for result in results:
-#         result_data = {'boxes': [],
-#                        'image_shape': result.orig_shape,
-#                        'image_path': result.path,
-#                        'save_dir': result.save_dir}
-#         boxes: ultralytics.engine.results.Boxes = result.boxes
-#         for box in boxes:
-#             box_data = {}
-#             coordinates = box.xyxy.cpu().detach().numpy().flatten().tolist()
-#             conf = box.conf
-#             obj_class = box.cls
-#             box_data['box_id'] = box.id
-#             box_data['coordinates'] = {'x1': coordinates[0],
-#                                         'x2': coordinates[1],
-#                                         'y1': coordinates[2],
-#                                         'y2': coordinates[3]}
-#             box_data['class'] = obj_class
-#             box_data['conf'] = conf
-#             result_data['boxes'].append(box_data)
+def inference_on_img(img: Image) -> List[Dict[str, Any]]:
+    """Performs inference on the trained model for the YOLOv8 architecture"""
+    results: ultralytics.engine.results.Results = model(source=img, show=False, conf=0.45)
+    result_data = []
+    for result in results:
+        result_data = json.loads(result.tojson())
 
-#         results_data.append(result_data)
-
-#     return results_data
+    return result_data

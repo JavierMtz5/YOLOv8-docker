565a46d167631e77e170c35aab6e1d8b00b31d5a
JavierMtz5 <jmartinezojeda5upv@gmail.com>
2024-06-29 03:23:14 +0200

    code cleaned and formatted in main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
+"""Main script for exposing the FastAPI API for YOLO inference"""
 import os
-import base64
-from typing import List
 from PIL import Image
 from io import BytesIO
+from typing import Dict, Any
 
-from fastapi import FastAPI, Query, File, UploadFile, Form
-from pydantic import BaseModel
+from fastapi import FastAPI, Query, File, UploadFile
 from app.model.model import inference_on_img, inference_on_path
 from app.model.model import __version__ as model_version
 
@@ -13,6 +12,7 @@ app = FastAPI()
 
 @app.get('/')
 def home():
+    """Ping method for checking API status"""
     return {
         'status_code': 200, 
         'health_check': 'OK', 
@@ -20,14 +20,22 @@ def home():
     }
 
 @app.get('/images')
-def get_available_images(path: str = Query(..., description='The path to search for images')):
+def get_available_images(
+    path: str = Query(..., description='The path to search for images')
+) -> Dict[str, Any]:
+    """Returns the list of available image files in the path received as query string"""
     available_images = []
-    # Parse the path, as it has $ instead of /
-    path = path.replace('$', '/')
+    path = path.replace('$', '/')   # Parse the path, as it has $ instead of /
+
     # Retrieve all images available for inference
     if os.path.exists(path):
         available_images = [filename for filename in os.listdir(path) if filename.endswith(('.png', '.jpg'))]
-        return {'status_code': 200, 'available_images': available_images, 'size': len(available_images)}
+        return {
+            'status_code': 200, 
+            'available_images': available_images, 
+            'size': len(available_images)
+        }
+
     return {
         'status_code': 400, 
         'message': 'The provided path does not exist', 
@@ -36,13 +44,17 @@ def get_available_images(path: str = Query(..., description='The path to search 
 
 @app.get('/detect')
 def detect(path: str = Query(..., description='The path to search for images')):
-    # Parse the path, as it has $ instead of /
-    path = path.replace('$', '/')
+    """Performs YOLO inference on all the images available in the path received as query string"""
+    path = path.replace('$', '/')   # Parse the path, as it has $ instead of /
+
+    # Perform inference for the images on the given path 
     try:
         inference_results_data = inference_on_path(imgs_path=path)
     except Exception as err:
         print(f'An error occurred while trying to perform inference. {err}')
-        return {'status_code': 500}
+        return {
+            'status_code': 500
+        }
 
     return {
         'status_code': 200, 
@@ -51,12 +63,16 @@ def detect(path: str = Query(..., description='The path to search for images')):
 
 @app.post('/detect_img')
 async def detect_img(img: UploadFile = File(...)):
+    """Performs YOLO inference on the received image"""
     if not img:
         return {'status_code': 400, 'message': 'No upload file sent'}
 
+    # Load the received image as PIL Image
     img_content = await img.read()
     image_stream = BytesIO(img_content)
     image = Image.open(image_stream)
+
+    # Perform inference for the received image
     inference_results_data = inference_on_img(img=image)
     return {
         'status_code': 200, 
